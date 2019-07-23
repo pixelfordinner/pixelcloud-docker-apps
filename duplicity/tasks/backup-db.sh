a2957a9125812cd62dcc2aba6ac2ea73c45aeb0e
Karl Fathi <karl.fathi@gmail.com>
2019-07-23 15:19:17 -0300

    Fix: Now dumping and then compressing to remain inside container volume

--- duplicity/tasks/backup-db.sh
+++ duplicity/tasks/backup-db.sh
@@ -12,12 +12,15 @@ for container in $containers
 do
     # Ensure dump dir exists
     $(which docker) exec $container mkdir -p ${dumps_dir}
-    # Clear old dumps (> 1 month)
+    # Clear old dumps (> 8 days)
     echo ">> [${container}] Clearing old dumps..."
-    $(which docker) exec $container find ${dumps_dir} -type f -name '*.sql' -mtime +8 -delete
+    $(which docker) exec $container find ${dumps_dir} -type f -name '*.sql' -name '*.sql.gz' -mtime +8 -delete
     echo ">> [${container}] Dumping database..."
-    # Generate new dumnp
-    $(which docker) exec $container wp db export - | gzip -7 -c > ${dumps_dir}/$(date +%Y.%m.%d).sql.gz
+    # Generate new dump
+    $(which docker) exec $container wp db export ${dumps_dir}/$(date +%Y.%m.%d).sql
+    echo ">> [${container}] Compressing dump..."
+    # Cmpressing dump
+    $(which docker) exec $container gzip -7 ${dumps_dir}/$(date +%Y.%m.%d).sql
     echo ">> [${container}] Done."
     echo
     echo
