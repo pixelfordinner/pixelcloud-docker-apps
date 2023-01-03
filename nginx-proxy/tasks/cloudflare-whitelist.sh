11e13a04b2a4361bee3bd614a90f0e8a3e443c57
Karl Fathi <karl.fathi@gmail.com>
2023-01-03 16:01:54 -0300

    Add: Cloudflare IPs whitelist task

---
+++ nginx-proxy/tasks/cloudflare-whitelist.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+CLOUDFLARE_FILE_PATH=/opt/apps/pixelcloud/nginx-proxy/volumes/nginx/conf.d/cloudflare.conf
+
+echo "#Cloudflare" > $CLOUDFLARE_FILE_PATH;
+echo "" >> $CLOUDFLARE_FILE_PATH;
+
+echo "# - IPv4" >> $CLOUDFLARE_FILE_PATH;
+for i in `curl -s -L https://www.cloudflare.com/ips-v4`; do
+    echo "set_real_ip_from $i;" >> $CLOUDFLARE_FILE_PATH;
+done
+
+echo "" >> $CLOUDFLARE_FILE_PATH;
+echo "# - IPv6" >> $CLOUDFLARE_FILE_PATH;
+for i in `curl -s -L https://www.cloudflare.com/ips-v6`; do
+    echo "set_real_ip_from $i;" >> $CLOUDFLARE_FILE_PATH;
+done
+
+echo "" >> $CLOUDFLARE_FILE_PATH;
+echo "real_ip_header CF-Connecting-IP;" >> $CLOUDFLARE_FILE_PATH;
